942141aff82f3f672fa96718181ef6fc210a9398
Nikita <timastan@gmail.com>
2025-10-07 14:12:08 +0300

    Added solution for 001_Two_Sum problem and initial README file

---
+++ README.md
@@ -0,0 +1,3 @@
+Hello!
+
+It is my repo with my solutions on the platform LeetCode. This README file will be improved after some time, when there will be more answers added!
---
+++ easy/001_Two_Sum.cpp
@@ -0,0 +1,28 @@
+// Problem: Two Sum
+//
+// Description:
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+//
+// Difficulty: Easy
+// Link: https://leetcode.com/problems/two-sum/description/
+// Time Complexity: 0(n^2)
+// Space Complexity: 0(1)
+// Runtime: 45ms
+// Memory: 13.99 MB
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int i, k;
+        for(i = 0; i < nums.size(); i++) {
+            for(k = i + 1; k < nums.size(); k++) {
+                if(nums[i] + nums[k] == target) {
+                return {i, k};
+            }
+            }
+        }
+        return {0};
+    }
+};
